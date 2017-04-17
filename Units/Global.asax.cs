67a75a40a1797d9c8ff2d919f55207ebf4c1ba3e
Michael VanderHelm <michaelv@attunix.com>
2017-04-17 16:10:22 -0700

    autofac

--- Units/Global.asax.cs
+++ Units/Global.asax.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Units.Models;
 
 namespace Units
 {
@@ -12,10 +17,28 @@ namespace Units
     {
         protected void Application_Start()
         {
+            SetupIOC();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void SetupIOC()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterType<GradeRepo>().As<IGradeRepository>();
+            builder.RegisterType<StudentRepo>().As<IStudentRepository>();
+            builder.RegisterType<CourseRepo>().As<ICourseRepository>();
+            builder.RegisterType<TodoRepo>().As<ITododRepository>();
+            builder.RegisterType<ApplicationDbContext>().As<DbContext>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
