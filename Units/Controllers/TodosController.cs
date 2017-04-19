fe09234522567bf70651e01d475371b64fa5e28f
Michael VanderHelm <michaelv@attunix.com>
2017-04-19 15:51:39 -0700

    todos ui

---
+++ Units/Controllers/TodosController.cs
@@ -0,0 +1,12 @@
+﻿using Units.Data;
+using Units.Data.Models;
+
+namespace Units.Controllers
+{
+    public class TodosController : BaseController<Todo>
+    {
+        public TodosController(ITododRepository repo) : base(repo)
+        {
+        }
+    }
+}
